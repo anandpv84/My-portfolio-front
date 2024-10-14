644857fdb7a9b295e713318b51dccec40ff0fc41
Anand pv <127194732+anandpv84@users.noreply.github.com>
2024-10-14 11:13:41 +0530

    Update Dashboard.jsx
    exi respons edit

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -54,7 +54,7 @@ function Dashboard() {
         alert("project added successfully")
         handleClose()
       } else (
-        alert(result.data.message)
+        alert(result.response.data.message)
       )
     }
   }
